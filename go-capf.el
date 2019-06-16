7ff05ce4b1c68dfc06f7789c7d8c1c57138d4ade
Philip K <philip@warpmail.net>
2019-06-16 22:06:15 +0200

    version bump

--- go-capf.el
+++ go-capf.el
@@ -1,7 +1,7 @@
 ;;; go-capf.el --- Completion-at-point backend for go -*- lexical-binding: t -*-
 
 ;; Author: Philip K. <philip@warpmail.net>
-;; Version: 0.1.0
+;; Version: 1.0.0
 ;; Keywords: languages, abbrev, convenience
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://git.sr.ht/~zge/go-capf
